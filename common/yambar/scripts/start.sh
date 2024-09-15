a3d51020e7971c5ee26a2daaa1abb914ed8991e1
Christopher Cavanaugh <christopher@chrcav.dev>
2024-09-14 22:16:26 -0400

    added multimonitor support, breaks laptop?

--- common/yambar/scripts/start.sh
+++ common/yambar/scripts/start.sh
@@ -4,4 +4,18 @@ killall yambar
 if [ "$(hostname)" = "mntpocketr" ]; then
 	alt_config="-c $HOME/.config/yambar/laptop.yml"
 fi
+monitors=$(wlr-randr | grep "^[^ ]" | awk '{ print$1 }')
+total=$(wlr-randr | grep "^[^ ]" | awk '{ print$1 }' | wc -l)
+
+if [ "$total" -gt "1" ]; then
+	for monitor in ${monitors}; do
+		if [ "$monitor" != "DP-1" ]; then
+			riverctl focus-output ${monitor}
+			yambar -c "$HOME/.config/yambar/condensed.yml" &
+			sleep 0.2
+		fi
+	done
+	riverctl focus-output DP-1
+fi
+
 yambar $alt_config &
